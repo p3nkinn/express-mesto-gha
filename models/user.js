d700bc344e9bc4858bea93fb0962f51166e188e9
Sergey Penkin <p3nkinn@gmail.com>
2022-08-12 19:02:25 +0300

    change length

--- models/user.js
+++ models/user.js
@@ -4,18 +4,18 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30
+    minLength: 2,
+    maxLength: 30,
   },
   about: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30
+    minLength: 2,
+    maxLength: 30,
   },
   avatar: {
     type: String,
-    required: true
+    required: true,
   }
 })
 
