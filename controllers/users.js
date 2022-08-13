9a1d09c6667aaf338ea90dbe7c7974ca9a3f1079
Sergey Penkin <p3nkinn@gmail.com>
2022-08-13 08:53:19 +0300

    error

--- controllers/users.js
+++ controllers/users.js
@@ -1,36 +1,45 @@
-const User = require('../models/user');
+const User = require("../models/user");
+const ERROR_CODE = 400;
 
 module.exports.getUser = (req, res) => {
   User.find({})
-    .then(user => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-}
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+    if (err.name === "SomeErrorName")
+    return res.status(ERROR_CODE).send({
+      message: "Запрашиваемый пользователь не найден",
+    });
+};
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.usersId)
-    .then(user => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  User.create({name, about, avatar})
-    .then(user => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-}
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+    if (err.name === "SomeErrorName")
+    return res.status(ERROR_CODE).send({
+      message: "Запрашиваемый пользователь не найден",
+    });
+};
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
   // обновим имя найденного по _id пользователя
   User.findByIdAndUpdate(req.params.usersId, { name, about })
-  .then(user => res.send({ data: user }))
-  .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-}
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
   // обновим имя найденного по _id пользователя
   User.findByIdAndUpdate(req.params.usersId, { avatar })
-  .then(user => res.send({ data: user }))
-  .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-}
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
