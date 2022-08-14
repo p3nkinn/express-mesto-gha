f24c11505664c4eca429a22e9273b280ab056614
Sergey Penkin <p3nkinn@gmail.com>
2022-08-14 22:51:26 +0300

    test

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,5 @@
 const User = require("../models/user");
 
-
 module.exports.getUser = (req, res) => {
   User.find({})
     .then((user) => res.status(200).send({ data: user }))
@@ -11,19 +10,21 @@ module.exports.getUser = (req, res) => {
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.usersId)
-  .orFail(() => {
-    throw new Error('Пользователь не найден')
-  })
+    .orFail(() => {
+      throw new Error("Пользователь не найден");
+    })
     .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(400)
-          .send({ message: 'Пользователь по указанному id не найден.'});
-      } else if (err.name === 'Error') {
-        res.status(404)
-          .send({ message: 'Пользователь по указанному id не найден в БД.'});
+      if (err.name === "CastError") {
+        res
+          .status(400)
+          .send({ message: "Пользователь по указанному id не найден." });
+      } else if (err.name === "Error") {
+        res
+          .status(404)
+          .send({ message: "Пользователь по указанному id не найден в БД." });
       } else {
-        res.status(500).send({ message: 'Произошла ошибка' });
+        res.status(500).send({ message: "Произошла ошибка" });
       }
     });
 };
@@ -33,43 +34,53 @@ module.exports.createUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(400)
-          .send({ message: 'Переданы некорректные данные при создании' });
+      if (err.name === "ValidationError") {
+        res
+          .status(400)
+          .send({ message: "Переданы некорректные данные при создании" });
       } else {
-        res.status(500).send({ message: 'Произошла ошибка' });
+        res.status(500).send({ message: "Произошла ошибка" });
       }
     });
 };
 
 module.exports.updateUser = (req, res) => {
-  const { name, about , avatar} = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about, avatar })
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true }
+  )
     .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(400)
-          .send({ message: ' Переданы некорректные данные при обновлении профиля.' });
-      } else if (err.name === 'Error') {
-        res.status(404)
-          .send({ message: 'Пользователь с указанным id не найден.' });
+      if (err.name === "ValidationError") {
+        res
+          .status(400)
+          .send({
+            message: " Переданы некорректные данные при обновлении профиля.",
+          });
       } else {
-        res.status(500).send({ message: 'Произошла ошибка' });
+        res.status(500).send({ message: "Произошла ошибка" });
       }
     });
 };
 
 module.exports.updateAvatar = (req, res) => {
-  const { name, about , avatar } = req.body;
+  const { avatar } = req.body;
   // обновим имя найденного по _id пользователя
-  User.findByIdAndUpdate(req.params.usersId, req.user._id, { name, about , avatar})
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true }
+  )
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(400)
-          .send({ message: 'Переданы некорректные данные при создании' });
+      if (err.name === "ValidationError") {
+        res
+          .status(400)
+          .send({ message: "Переданы некорректные данные при создании" });
       } else {
-        res.status(500).send({ message: 'Произошла ошибка' });
+        res.status(500).send({ message: "Произошла ошибка" });
       }
     });
 };
