4cbb6b442280096536dbef781e3f0f753145c9d2
Sergey Penkin <p3nkinn@gmail.com>
2022-08-15 01:12:52 +0300

    test

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use((req, res, next) => {
   req.user = {
     _id: '62f94cac09ce15b5df1c49a2' // вставьте сюда _id созданного в предыдущем пункте пользователя
@@ -20,8 +21,9 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
-app.use((req, res, next) => {
-  res.status(404).send('Sorry cant find that!');
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: "Страница не найдена" });
+  next(err);
 });
 
 app.listen(PORT, () => {
