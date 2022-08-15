00ffb27c1bac73921404642135082ffe0c06dcc2
Sergey Penkin <p3nkinn@gmail.com>
2022-08-15 15:36:54 +0300

    test

--- app.js
+++ app.js
@@ -21,9 +21,13 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: "Страница не найдена" });
-  next(err);
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(PORT, () => {
