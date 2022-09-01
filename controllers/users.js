44d16549d2384d180ac91bc9235df9495265c408
Sergey Penkin <p3nkinn@gmail.com>
2022-09-01 22:31:23 +0300

    test

--- controllers/users.js
+++ controllers/users.js
@@ -28,7 +28,7 @@ module.exports.getUser = (req, res, next) => {
 };
 
 module.exports.getCurrentUser = (req, res, next) => {
-  User.findById(req.params._id)
+  User.findById(req.user._id)
     .orFail()
     .catch(() => {
       throw new NotFound('Нет пользователя с таким id');
@@ -39,16 +39,15 @@ module.exports.getCurrentUser = (req, res, next) => {
 
 module.exports.getUserById = (req, res, next) => {
   User.findById(req.params.usersId)
-    .then((user) => {
-      if (user) {
-        res.send({ data: user });
-      } else {
-        throw new NotFound('Данные по указанному id не найдена в БД.');
-      }
+    .orFail(() => {
+      throw new Error('NotFound');
     })
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'CastError') {
         throw new BadRequest('Передан некорректный id');
+      } else if (err.message === 'NotFound') {
+        throw new NotFound('Данные по указанному id не найдена в БД.');
       }
     })
     .catch((err) => next(err));
