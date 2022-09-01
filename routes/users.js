3b72e6d5948c41beebe8da8bf94454025786312f
Sergey Penkin <p3nkinn@gmail.com>
2022-09-01 22:39:43 +0300

    test

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.get(
   auth,
   celebrate({
     params: Joi.object().keys({
-      userId: Joi.string().alphanum().length(24),
+      usersId: Joi.string().alphanum().length(24),
     }),
   }),
   getUserById,
